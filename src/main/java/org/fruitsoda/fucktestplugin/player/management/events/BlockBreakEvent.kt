59ffe7070af4a551119244106de2ef04be605312
imgyeonghun <rhkdlfaktdndb92@gmail.com>
2024-06-09 21:23:21 +0900

    test add line

--- src/main/java/org/fruitsoda/fucktestplugin/player/management/events/BlockBreakEvent.kt
+++ src/main/java/org/fruitsoda/fucktestplugin/player/management/events/BlockBreakEvent.kt
@@ -5,12 +5,12 @@ import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
 
-object BlockBreakEvent:Listener {
+object BlockBreakEvent:Listener, JavaPlugin(){
     @EventHandler
     fun onBreakBlockEvent(e: BlockBreakEvent){
-//        //val msg1:String = this.config.getString("블록메시지.첫번째")!!
-//        //val msg2:String = this.config.getString("블록메시지.두번째")!!
-//        e.player.sendMessage(msg1)
-//        e.player.sendMessage(msg2)
+        val msg1:String = this.config.getString("블록메시지.첫번째")!!
+        val msg2:String = this.config.getString("블록메시지.두번째")!!
+        e.player.sendMessage(msg1)
+        e.player.sendMessage(msg2)
     }
 }
