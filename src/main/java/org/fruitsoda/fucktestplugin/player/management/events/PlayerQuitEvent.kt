7b96ae235056f9867e5daab2244110b1e035b5bb
imgyeonghun <rhkdlfaktdndb92@gmail.com>
2024-06-10 06:07:13 +0900

    add line PlayerQuitEvent.kt

--- src/main/java/org/fruitsoda/fucktestplugin/player/management/events/PlayerQuitEvent.kt
+++ src/main/java/org/fruitsoda/fucktestplugin/player/management/events/PlayerQuitEvent.kt
@@ -3,11 +3,21 @@ package org.fruitsoda.fucktestplugin.player.management.events
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerQuitEvent
+import org.fruitsoda.fucktestplugin.Main
 import org.fruitsoda.fucktestplugin.player.management.PlayerManager
 
 object PlayerQuitEvent : Listener {
     @EventHandler
     fun onPlayerQuitEvent(e: PlayerQuitEvent){
-        //PlayerManager.deletePlayer(e.player)
+        val playerName:String?
+        PlayerManager.getPlayerData(e.player).let {
+            playerName = it?.displayName
+        }
+        if (playerName == null){
+            Main.instance?.logger?.info("player name is null")
+        }else{
+            Main.instance?.logger?.info("player name is ${playerName}")
+        }
+
     }
 }
