6520e2117f573b6da6b37a8cca05237edc6d106e
imgyeonghun <rhkdlfaktdndb92@gmail.com>
2024-06-09 21:28:25 +0900

    test add Main.instance?.config? line

--- src/main/java/org/fruitsoda/fucktestplugin/player/management/events/BlockBreakEvent.kt
+++ src/main/java/org/fruitsoda/fucktestplugin/player/management/events/BlockBreakEvent.kt
@@ -4,12 +4,14 @@ import org.bukkit.event.EventHandler
 import org.bukkit.plugin.java.JavaPlugin
 import org.bukkit.event.Listener
 import org.bukkit.event.block.BlockBreakEvent
+import org.fruitsoda.fucktestplugin.Main
 
-object BlockBreakEvent:Listener, JavaPlugin(){
+object BlockBreakEvent:Listener{
+
     @EventHandler
     fun onBreakBlockEvent(e: BlockBreakEvent){
-        val msg1:String = this.config.getString("블록메시지.첫번째")!!
-        val msg2:String = this.config.getString("블록메시지.두번째")!!
+        val msg1:String = Main.instance?.config?.getString("블록메시지.첫번째")!!
+        val msg2:String = Main.instance?.config?.getString("블록메시지.두번째")!!
         e.player.sendMessage(msg1)
         e.player.sendMessage(msg2)
     }
